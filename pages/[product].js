1399cf439e8c13b49ea4d3e399685fa839ad85b3
marcelreit <marcel.reiter2077@gmail.com>
2023-09-02 18:45:11 +0200

    major first update

---
+++ pages/[product].js
@@ -0,0 +1,22 @@
+import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+export default function Product() {
+    const {
+        query: { product },
+    } = useRouter();
+    return (
+        <div className={styles.container}>
+            <motion.h1 layoutId="header">{product}</motion.h1>
+            <Link href="/">
+                    <motion.img
+                        layoutId={product}
+                        className={styles["big-image"]}
+                        src={product + ".jpg"}
+                    />
+            </Link>
+        </div>
+    );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,26 @@
-import Head from "next/head";
+import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Home() {
+
+
   return (
-    <div className="home">
-      <Link href="/explore">
-        <a className="explore-btn">EXPLORE</a>
-      </Link>
-    </div>
+      <div className={styles.container}>
+          <motion.h1 layoutId="header">My shop</motion.h1>
+          <div className={styles["product-container"]}>
+              {["shoe", "watch", "headphones"].map((product) => (
+                  <Link href={product} key={product}>
+                          <motion.img
+                              layoutId={product}
+                              className={styles.image}
+                              src={product + ".jpg"}
+                              animate={{ scale: 1 }}
+                              whileHover={{ scale: 1.1 }}
+                          />
+                  </Link>
+              ))}
+          </div>
+      </div>
   );
 }
