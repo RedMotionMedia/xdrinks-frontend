b95a3313bc06c7a89b72edc21f184f0849aa80ea
marcel-gepardec <marcel.reiter@gepardec.at>
2024-09-20 15:48:41 +0200

    test pipeline 2

--- pages/contact.js
+++ pages/contact.js
@@ -4,7 +4,7 @@ export default function Contact() {
     return(
         <Page>
             <div className="h-screen flex items-center justify-center pb-10">
-                    <h1 className="lg:text-8xl text-4xl">Contact Page!</h1>
+                    <h1 className="lg:text-8xl text-4xl">Contact Page</h1>
             </div>
         </Page>
     )
